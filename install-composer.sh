dc4c29a4a23eb8719694f9180828323755e7a2db
soregashi-27 <tomo.soul.15@gmail.com>
2021-08-01 10:04:03 +0900

    gitignoreを追加

--- install-composer.sh
+++ install-composer.sh
@@ -4,7 +4,7 @@ EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
 
-if["$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE"]
+if ["$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE"]
 then
     >&2 echo 'ERROR: Invalid installer signature'
     rm composer-setup.php
