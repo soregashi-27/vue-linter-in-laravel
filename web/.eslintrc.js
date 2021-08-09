665f81b2398b225d5f3934ae6ef33b1ece1ad684
soregashi-27 <tomo.soul.15@gmail.com>
2021-08-10 08:17:34 +0900

    Prettierのエラーパターンを追加

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -13,6 +13,20 @@ module.exports = {
     'vue/prop-name-casing': 'off',
     'no-console': 'off',
     'no-unused-vars': 'off',
-    camelcase: 'off'
+    camelcase: 'off',
+
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 120,
+        tabWidth: 2,
+        useTabs: false,
+        singleQuote: true,
+        trailingComma: 'all',
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        semi: false
+      }
+    ]
   }
 }
